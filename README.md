390f729e5d5f761420dd4e9058b7383543eadba2
lexipenia <cst.fenwick@gmail.com>
2020-09-10 21:20:27 +0200

    full directory explorer + dl from different dirs

--- README.md
+++ README.md
@@ -16,9 +16,9 @@ Fill in configuration details in the `config.py` file.
 
 `remoteDir` can be used to navigate automatically to a specific folder on your server where downloads are located.
 
-The target directory for downloads is normally set at the beginning by dragging/dropping. You can modify the program to skip this step and specify `targetDir` in the config file if you wish.
+The target directory for all downloads is normally set at the beginning by dragging/dropping. You can modify the program to skip this step and specify `targetDir` in the config file if you wish.
 
-Simply run the program from the command line:
+Run the program from the command line:
 
 ```
 $ python sftp-resume.py
--- sftp-resume.py
+++ sftp-resume.py
@@ -11,80 +11,114 @@ from config import *
 
 # format for storing info on each individual file, including its full path
 class File:
-  def __init__(self,a,b,c):
-    self.name = a
-    self.path = b
-    self.size = c
+    def __init__(self,a,b,c):
+        self.name = a
+        self.path = b
+        self.size = c
+    def __eq__(self, other):                        # control for items already in queue
+        return self.__dict__ == other.__dict__
 
 # main function: connect and then select the files to download
 # actual downloads create a new SFTP connection in order to resume on break
 def main():
 
     # remove these two lines if specifying targetDir from config
-    global targetDir 
+    global targetDir
     targetDir = input("Drag and drop to select target local directory:\n").replace("\\", "").strip()
-
-    global remoteDir # allow us to change this when navigating
-
+    
     print("Connecting to the server…")
+
+    # collect downloads + any notifications/errors
+    downloads_list = []
+    added = ""
+    error = ""
+    global already_added
+    already_added = ""
 
     with createSFTPClient(myHostname, myPort, myUsername, myPassword) as sftp:
 
         print("Connection established successfully.")
         sftp.chdir(remoteDir)
+        global remoteDirFull
+        remoteDirFull = sftp.getcwd()   # get the full path; used for truncating local filepaths later
 
-        # select the base directory we want to download from, then select files; repeat loop on error
+        # display the current selected directory then loop to get user input and choose what to do next; break loop to initiate downloads
         while(True):
 
-            directory_structure = sftp.listdir_attr()
+            # list the directory, give update on the download list and print any errors/updates
+            # catch the permission error here at start of loop
+            try:
+                directory_structure = sftp.listdir_attr()
+            except Exception as e:
+                error = "\nError changing directory: " + str(e) + "\nReturning to main directory."
+                sftp.chdir(remoteDirFull)
+                continue
+
             directory_structure.sort(key = lambda x: x.st_mtime, reverse=True)  # sort by date, newest first
+            print("\nCurrent directory contents:")
             for x in reversed(directory_structure):                             
                 print(directory_structure.index(x),"|",getSize(x),"|", x.filename)
+            print("\nCurrent directory path:",sftp.getcwd())
+            print("Downloads list: {} items, {} total".format(len(downloads_list),tidySize(sum(x.size for x in downloads_list))))
 
-            print("\nCurrent directory:",remoteDir)
+            if added != "":
+                print(added)
+                added = ""
+            if already_added != "":
+                print(already_added)
+                already_added = ""
+            if error != "":
+                print(error)
+                error = ""
 
-            nav_choice = input("\nType \"cd\" and a directory number (or ..) to change directory or enter files to download separated by comma, e.g. \"cd 12\", \"cd ..\" or \"1,3,5\".\n")
+            # process the user's input
+            nav_choice = input("\n\"cd\": change directory. \"add\": add to download queue. \"clear\": clear queue. \"dl\": start downloading.\nExample commands: \"cd 12\", \"cd ..\", \"add 2\", \"add 1,3,5\", \"add all\", \"dl\".\n")
 
-            if nav_choice[:3] == "cd ":
-                try:
+            try:
+
+                if nav_choice[:3] == "cd ":                     # change directory
                     if nav_choice == "cd ..":
                         sftp.chdir("..")
-                        remoteDir = sftp.getcwd()   # we must reset this so the "download" function picks it up
                     else:
                         index = int(nav_choice[3:])
                         sftp.chdir(directory_structure[index].filename)
-                        remoteDir = sftp.getcwd()    
-                except Exception as e:
-                    print("Input error:\n", str(e))
-                    print("Let’s try again in 5 seconds…")
-                    sleep(5)
-                    continue
-            else:
-                try:
-                    # collect names of the root files/folders to download
+
+                elif nav_choice[:4] == "add ":                   # add files to list
                     root_folders = [] 
-                    indices = nav_choice.split(",")
+                    if nav_choice == "add all":
+                        indices = range(0,len(directory_structure))
+                    else:
+                        indices = nav_choice[4:].split(",")
+                    added = "\n{} file(s)/folder(s) added:".format(len(indices))
                     for index in indices:
                         root_folders.append(directory_structure[int(index)].filename)
-
-                    # get all the required info on each file for the download function
-                    print("Adding files to download queue…")
-                    all_files = []
+                        added += "\n" + directory_structure[int(index)].filename
+                    print("Adding {} file(s)/folder(s) to download queue…".format(len(indices)))
                     for item in root_folders:
-                        getFileInfo(item,"",all_files,sftp)  # pass on empty base path + sftp client
+                        print(item)
+                        getFileInfo(item,"",downloads_list,sftp)  # pass on empty base path + sftp client                        
+
+                elif nav_choice == "clear":                       # clear downloads
+                    print("Clearing downloads list…")
+                    downloads_list = []
+                    sleep(1)
+
+                elif nav_choice[:2] == "dl":                      # initiate downloads
                     break
-                except Exception as e:
-                    print("Input error:\n", str(e))
-                    print("Let’s try again in 5 seconds…")
-                    sleep(5)
-                    continue
+
+                else:
+                    error = "\nCommand not recognized: \"" + nav_choice + "\""
+
+            except Exception as e:
+                error = "\nError during input: " + str(e)
+                continue
 
         # if connection breaks keep trying until we have all the files!
         while(True):
             try:
-                download(all_files)
+                download(downloads_list)
                 print("All downloads finished.")
-                exit()
+                exit(0)
             except Exception as e:
                 if str(e).strip() == "Server connection dropped:":
                     print("Connection error occurred. Retrying in 10 seconds…")
@@ -92,7 +126,7 @@ def main():
                 else:
                     print("An error occurred:\n" + str(e))
                     print("Quitting…")
-                    exit()
+                    exit(1)
 
 # create a paramiko SFTP client
 def createSFTPClient(host, port, username, password):
@@ -106,7 +140,7 @@ def getSize(dir):
         size_string = tidySize(dir.st_size)
         if len(size_string) < 9:
             diff = 9 - len(size_string)
-            return size_string + " "*diff
+            return size_string + " " * diff
         else:
             return size_string
     else:
@@ -134,19 +168,26 @@ def getFileInfo(filename, base_path, target_list, sftp_client):
     elif file_type == 16893 or file_type == 16877:
         new_dir = sftp_client.listdir(file_path)
         for x in new_dir:
-            getFileInfo(x,file_path + "/",target_list,sftp_client)  # turn filepath+name into new base bath
+            getFileInfo(x,file_path + "/",target_list,sftp_client)  # turn filepath into new base bath
 
 # create a new "File" object and add it to the downloads list
 def addFileToList(filename,file_path,target_list,sftp_client):
 
+    global already_added
+
     new_file = File(
         filename,
-        file_path,
+        sftp_client.getcwd() + "/" + file_path,
         sftp_client.stat(file_path).st_size
     )
 
-    target_list.append(new_file)
-
+    for x in target_list:
+        if x == new_file:
+            already_added += "\nAlready in queue: " + new_file.name
+            break
+    else:
+        target_list.append(new_file)
+        
 # download all the files in the list, creating a new SFTP client each time the function runs
 def download(file_list):
 
@@ -165,7 +206,6 @@ def download(file_list):
     with createSFTPClient(myHostname, myPort, myUsername, myPassword) as sftp:
 
         print("Connection established successfully.")
-        sftp.chdir(remoteDir)
 
         # set up the progress bar here and pass it into the callback function
         with alive_bar(int(progress["total_size"]/1000000), bar = "smooth", spinner = "pointer", manual=True) as bar:
@@ -178,7 +218,7 @@ def download(file_list):
                 progress["last_file_total"] = 0
                 print("Downloading {} of {} | {} | {}".format(current_item_number,len(file_list),tidySize(item.size),item.name))
                 
-                local_path = targetDir + "/" + item.path
+                local_path = targetDir + tidyPath(item.path)
 
                 # get local size if file exists; if it doesn't, create directories + download         
                 if os.path.isfile(local_path):                   
@@ -199,6 +239,29 @@ def download(file_list):
                         sftp._transfer_with_callback(reader=remote_file, writer=local_file, file_size=remote_size, callback=lambda x,y: updateProgress(x,bar))
                 
             bar(1)    # make sure bar ends on 100% if we have iterated over whole list
+
+# remove the unwanted parts of the remoteDir path 
+def tidyPath(remote_path):
+
+    diff = False
+    for i in range(0,len(remoteDirFull)):
+        if remoteDirFull[i] != remote_path[i]:
+            cut = i
+            diff = True
+            break
+    if diff == False:               # catch the case where the whole string is contained
+        cut = len(remoteDirFull)    # NB. these will contain "/", others won't
+
+    if cut == 0:                    # find the first relevant "/" in the string
+        return remote_path
+    else:
+        first_half = remote_path[:cut+1]
+        dir_begin = 0
+        for i in range(len(first_half)-1,-1,-1):
+            if first_half[i] == "/":
+                dir_begin = i
+                break
+        return remote_path[dir_begin:]
 
 # update the progress bar and track our total progress
 def updateProgress(x,bar):
